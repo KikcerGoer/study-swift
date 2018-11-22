f5c7164afce8702c98d64912acd0ebcea8706e25
King <43101667+KikcerGoer@users.noreply.github.com>
2018-11-22 21:23:13 +0800

    交代下去吧

--- code/exp4.swift
+++ code/exp4.swift
@@ -146,3 +146,45 @@ let ct = Controller()
 
 PlaygroundPage.current.liveView = ct
 
+
+// 实验4 (3)代码版hello world（在视图控制器中加入代码）
+/*
+  a)代码中生成label（outlet）和button；
+  b)将label和button加入根view中；
+  c)button添加像self（target）发射action（selector）的操作；
+  d)实现clicked响应代码（selector）；
+*/
+class MyController:UIViewController{
+    var lable:UILabel!
+    
+    @IBAction func clicked(){
+        print("i am clicked");
+        lable.text = "Hello World";
+    }
+    
+    
+    override func loadView() {
+        view = UIView(frame:CGRect(x:0,y:0,width:400,height:700));
+        view.backgroundColor = UIColor.red;
+        
+        lable = UILabel(frame:CGRect(x:50,y:100,width:300,height:50));
+        lable.backgroundColor = UIColor.blue;
+        lable.textColor = UIColor.white;
+        lable.textAlignment  = .center;
+        lable.text = "点击发现我要说的话";
+        
+        view.addSubview(lable);
+        
+        let button = UIButton(frame:CGRect(x:140,y:500,width:120,height:40))
+        button.setTitle("来呀快活呀", for: .normal);
+        button.addTarget(self, action: #selector(MyController.clicked), for: UIControlEvents.touchUpInside);
+        button.backgroundColor = UIColor.white;
+        button.titleLabel?.textColor = UIColor.black;
+        view.addSubview(button)
+    }
+    
+}
+
+let ct_3 = MyController()
+
+PlaygroundPage.current.liveView = ct_3
